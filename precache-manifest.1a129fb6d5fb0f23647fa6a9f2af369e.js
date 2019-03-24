13e2b7f7f53aec8867ff96acd98854336ad97e21
davemarch <davemarchaudio@gmail.com>
2019-03-24 19:32:17 +0000

    Updates

---
+++ precache-manifest.1a129fb6d5fb0f23647fa6a9f2af369e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d7b8b135d04ba3913063fdced10330b",
+    "url": "/stubucks/static/media/StubucksHero.1d7b8b13.jpg"
+  },
+  {
+    "revision": "ff2db08cee8bd280eec8",
+    "url": "/stubucks/static/css/main.91c5a005.chunk.css"
+  },
+  {
+    "revision": "e84069ab57254b164cda",
+    "url": "/stubucks/static/js/runtime~main.2c0cf6f9.js"
+  },
+  {
+    "revision": "eb72443f7681268b38d3",
+    "url": "/stubucks/static/js/2.fcdef496.chunk.js"
+  },
+  {
+    "revision": "a70b01df3eb52a4db6264b92c0a975a6",
+    "url": "/stubucks/static/media/stubucks-logo.a70b01df.png"
+  },
+  {
+    "revision": "ff2db08cee8bd280eec8",
+    "url": "/stubucks/static/js/main.ab61ff80.chunk.js"
+  },
+  {
+    "revision": "5e00badedb58df5b57325e03227c2e9b",
+    "url": "/stubucks/static/media/StubrewIcedCoffee.5e00bade.png"
+  },
+  {
+    "revision": "5fb42db7331e6d7b47ff9f8ef4409dc7",
+    "url": "/stubucks/static/media/buyCoffee.5fb42db7.jpg"
+  },
+  {
+    "revision": "f872f55e4f418ab3c0e199376e62cd0a",
+    "url": "/stubucks/static/media/buyTea.f872f55e.jpg"
+  },
+  {
+    "revision": "80bb634e39454a7d66bb01421c0ce300",
+    "url": "/stubucks/static/media/buyEquipment.80bb634e.png"
+  },
+  {
+    "revision": "c7aa1b19675dc004a33a94719ace0b1d",
+    "url": "/stubucks/index.html"
+  }
+];
